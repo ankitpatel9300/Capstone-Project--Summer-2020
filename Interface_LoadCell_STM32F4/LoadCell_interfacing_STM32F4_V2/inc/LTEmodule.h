af6950fa96e8b58a7dbae775a3d406b304fb2a67
ankitpatel9300 <ankitpatel9300@gmail.com>
2020-08-08 16:22:15 -0400

    Interfacing Loadcell and LTE module with STM32F4

---
+++ Interface_LoadCell_STM32F4/LoadCell_interfacing_STM32F4_V2/inc/LTEmodule.h
@@ -0,0 +1,25 @@
+/**
+  ******************************************************************************
+  * @file    LTEmodule.h
+  * @author  Ankit
+  * @version V1.0
+  * @date    07-August-2020
+  * @brief   This file contains all the functions prototypes for the LTE module interface with STM32F4
+  ******************************************************************************/
+
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __LTEmodule_H
+#define __LTEmodule_H
+
+static void prvSetupLTEmodule(void);
+
+
+
+
+
+
+
+
+
+#endif
